32ce7b8efe78625bbbfc57cc416af58c8e687fd9
Savio <savio_ls@hotmail.com>
2020-12-03 00:10:08 -0300

    Correnção na tela de Relatorio consolidado.

--- Sport_Tads/src/main/java/br/sport/tads/utils/PopularTabelas.java
+++ Sport_Tads/src/main/java/br/sport/tads/utils/PopularTabelas.java
@@ -17,9 +17,11 @@ public class PopularTabelas {
         inserirClientes();
         inserirFiliais();
         inserirProdutos();
+        //Somente os Ti e atravez dele e possivel cadastrar novos
         inserirColaboradores();
         inserirVendas();
         inserirItensVendas();
+        
     }
 
     public static void inserirClientes() {
@@ -49,11 +51,9 @@ public class PopularTabelas {
             String query = "INSERT INTO tb_filial \n"
                     + "(nomeFilial, estado, cidade, status)\n"
                     + "VALUES\n"
-                    + "('Feira de Santana', 'BA', 'Feira de Santana', 1),\n"
-                    + "('Rio de Janeiro', 'RJ', 'Rio de Janeiro', 1),\n"
-                    + "('Belo Horizonte', 'MG', 'Belo Horizonte', 1),\n"
-                    + "('Porto Alegre', 'RS', 'Porto Alegre', 1),\n"
-                    + "('Campo Grande', 'MS', 'Campo Grande', 1)";
+                    + "('Matriz SP', 'SP', 'Sao Paulo', 1),\n"
+                    + "('Campina Grande', 'PB', 'Paraiba', 1),\n"
+                    + "('Joinville', 'SC', 'Santa catarina', 1)";
             PreparedStatement ps = conexao.prepareStatement(query);
             ps.execute(query);
         } catch (ClassNotFoundException ex) {
@@ -71,21 +71,67 @@ public class PopularTabelas {
             String query = "INSERT INTO tb_produto\n"
                     + "(codFilial, nomeProduto, marcaProduto, categoriaProduto, valorProduto, quantidadeEmEstoque)\n"
                     + "VALUES\n"
-                    + "(1, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 10),\n"
-                    + "(1, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 10),\n"
-                    + "(1, 'Luvas de Goleiro', 'Penalty', 'Futebol', 97.49, 10),\n"
-                    + "(2, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 10),\n"
-                    + "(2, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 10),\n"
-                    + "(2, 'Luvas de Goleiro', 'Penalty', 'Futebol', 97.49, 10),\n"
-                    + "(3, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 10),\n"
-                    + "(3, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 10),\n"
-                    + "(3, 'Luvas de Goleiro', 'Penalty', 'Futebol', 97.49, 10),\n"
-                    + "(4, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 10),\n"
-                    + "(4, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 10),\n"
-                    + "(4, 'Luvas de Goleiro', 'Penalty', 'Futebol', 97.49, 10),\n"
-                    + "(5, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 10),\n"
-                    + "(5, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 10),\n"
-                    + "(5, 'Luvas de Goleiro', 'Penalty', 'Futebol', 97.49, 10)";
+                    + "(1, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 30),\n"
+                    + "(1, 'Caneleira', 'Penalty', 'Futebol', 39.99, 30),\n"
+                    + "(1, 'Luvas de Goleiro', 'Penalty', 'Futebol', 57.49, 30),\n"
+                    
+                    + "(1, 'Tenis cano alto', 'Adidas', 'Basquete', 99.99, 30),\n"
+                    + "(1, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 30),\n"
+                    + "(1, 'Meia cano alto', 'Penalty', 'Basquete', 97.49, 30),\n"
+                    
+                    + "(1, 'Halteres', 'Oxer', 'Fitness', 37.99, 30),\n"
+                    + "(1, 'Caneleira de peso 2kg', 'Domyos', 'Fitness', 44.99, 30),\n"
+                    + "(1, 'Tapete baixa densidade', 'Domyos', 'Fitness', 24.49, 30),\n"
+                    
+                    + "(1, 'Kimonos', 'Torah', 'Marciais', 109.99, 30),\n"
+                    + "(1, 'Luva de Boxes', 'Outshock', 'Marciais', 159.99, 20),\n"
+                    + "(1, 'Corda de Pular', 'Outshock', 'Marciais', 45.49, 10),\n"
+                    
+                    + "(1, 'Bola de Volei', 'Allsix', 'Volei', 99.99, 10),\n"
+                    + "(1, 'Rede', 'Penalty', 'Volei', 49.99, 10),\n"
+                    + "(1, 'Bola Volei de quadra', 'Allsix', 'Volei', 77.49, 10)"
+                    
+                    + "(2, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 30),\n"
+                    + "(2, 'Caneleira', 'Penalty', 'Futebol', 39.99, 30),\n"
+                    + "(2, 'Luvas de Goleiro', 'Penalty', 'Futebol', 57.49, 30),\n"
+                    
+                    + "(2, 'Tenis cano alto', 'Adidas', 'Basquete', 99.99, 30),\n"
+                    + "(2, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 30),\n"
+                    + "(2, 'Meia cano alto', 'Penalty', 'Basquete', 97.49, 30),\n"
+                    
+                    + "(2, 'Halteres', 'Oxer', 'Fitness', 37.99, 30),\n"
+                    + "(2, 'Caneleira de peso 2kg', 'Domyos', 'Fitness', 44.99, 30),\n"
+                    + "(2, 'Tapete baixa densidade', 'Domyos', 'Fitness', 24.49, 30),\n"
+                    
+                    + "(2, 'Kimonos', 'Torah', 'Marciais', 109.99, 30),\n"
+                    + "(2, 'Luva de Boxes', 'Outshock', 'Marciais', 159.99, 20),\n"
+                    + "(2, 'Corda de Pular', 'Outshock', 'Marciais', 45.49, 10),\n"
+                    
+                    + "(2, 'Bola de Volei', 'Allsix', 'Volei', 99.99, 10),\n"
+                    + "(2, 'Rede', 'Penalty', 'Volei', 49.99, 10),\n"
+                    + "(2, 'Bola Volei de quadra', 'Allsix', 'Volei', 77.49, 10)\n"
+                                        
+                    + "(3, 'Bola de Futebol de Campo', 'Adidas', 'Futebol', 99.99, 30),\n"
+                    + "(3, 'Caneleira', 'Penalty', 'Futebol', 39.99, 30),\n"
+                    + "(3, 'Luvas de Goleiro', 'Penalty', 'Futebol', 57.49, 30),\n"
+                    
+                    + "(3, 'Tenis cano alto', 'Adidas', 'Basquete', 99.99, 30),\n"
+                    + "(3, 'Bola de Basquete', 'Penalty', 'Basquete', 49.99, 30),\n"
+                    + "(3, 'Meia cano alto', 'Penalty', 'Basquete', 97.49, 30),\n"
+                    
+                    + "(3, 'Halteres', 'Oxer', 'Fitness', 37.99, 30),\n"
+                    + "(3, 'Caneleira de peso 2kg', 'Domyos', 'Fitness', 44.99, 30),\n"
+                    + "(3, 'Tapete baixa densidade', 'Domyos', 'Fitness', 24.49, 30),\n"
+                    
+                    + "(3, 'Kimonos', 'Torah', 'Marciais', 109.99, 30),\n"
+                    + "(3, 'Luva de Boxes', 'Outshock', 'Marciais', 159.99, 20),\n"
+                    + "(3, 'Corda de Pular', 'Outshock', 'Marciais', 45.49, 10),\n"
+                    
+                    + "(3, 'Bola de Volei', 'Allsix', 'Volei', 99.99, 10),\n"
+                    + "(3, 'Rede', 'Penalty', 'Volei', 49.99, 10),\n"
+                    + "(3, 'Bola Volei de quadra', 'Allsix', 'Volei', 77.49, 10)";
+            
+                    
             PreparedStatement ps = conexao.prepareStatement(query);
             ps.execute(query);
         } catch (ClassNotFoundException ex) {
@@ -100,31 +146,22 @@ public class PopularTabelas {
     public static void inserirColaboradores() {
 
         Colaborador colaborador1 = new Colaborador("Benedita Raimunda Bernardes", "24292790965", "Praça Bento Gonçalves", 325,
-                13870319, "São João da Boa Vista", "Analista de Sistemas", 2, "brbernardes", "123456789", "TI");
+                13870319, "Sao Paulo", "Analista de Sistemas", 1, "Ti_01", "123", "TI");
         colaborador1.setSenha(colaborador1.codificarSenha(colaborador1.getSenha()));
 
         Colaborador colaborador2 = new Colaborador("Maria Aurora da Mata", "90508119308", "Rua São Marcos", 298,
-                69915428, "Rio Branco", "Vendedor(a)", 1, "madmata", "123456789", "VENDAS");
+                69915428, "Campina Grande", "Analista de Sistemas", 2, "Ti_02", "123\n" +
+                "", "TI");
         colaborador2.setSenha(colaborador2.codificarSenha(colaborador2.getSenha()));
 
         Colaborador colaborador3 = new Colaborador("Mateus Kaique Caua Drumond", "85701237796", "Rua Otacilio Mangueira Ramalho", 458,
-                58036836, "João Pessoa", "Analista de Estoque", 3, "mkcdrumond", "123456789", "BACKOFFICE");
+                58036836, "Join ville", "Analista de Sistemas", 3, "Ti_02", "123", "TI");
         colaborador3.setSenha(colaborador3.codificarSenha(colaborador3.getSenha()));
-
-        Colaborador colaborador4 = new Colaborador("Clara Simone Rezende", "98345164790", "Travessa L4 da Fonte Nova", 1474,
-                68928208, "Santana", "Gerente Regional", 4, "csrezende", "123456789", "REGIONAL");
-        colaborador4.setSenha(colaborador4.codificarSenha(colaborador4.getSenha()));
-
-        Colaborador colaborador5 = new Colaborador("Hadassa Malu Regina da Mata", "77377115700", "Rua Primeiro de Janeiro", 1432,
-                68908874, "Macapá", "Gerente Global", 5, "hmrdmata", "123456789", "GLOBAL");
-        colaborador5.setSenha(colaborador5.codificarSenha(colaborador5.getSenha()));
 
         try {
             ColaboradorDAO.addColadorador(colaborador1);
             ColaboradorDAO.addColadorador(colaborador2);
             ColaboradorDAO.addColadorador(colaborador3);
-            ColaboradorDAO.addColadorador(colaborador4);
-            ColaboradorDAO.addColadorador(colaborador5);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(ServletBD.class.getName()).
                     log(Level.SEVERE, null, ex);
